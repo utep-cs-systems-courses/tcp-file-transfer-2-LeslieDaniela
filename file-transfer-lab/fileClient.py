2c5a105bfb6f15a408468eccb086c122f264ae45
LeslieDaniela <33047756+LeslieDaniela@users.noreply.github.com>
2020-12-03 11:35:35 -0700

    Added Lab Folder for vm

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+import os, re, socket, sys
+
+sys.path.append("../lib") # for params
+import params
+from framedSock import framedSend, framedReceive
+
+PATH = "Send/"
+
+def fileClient():
+    switchesVarDefaults = (
+        (('1', '--server'), 'server', "127.0.0.1:50001"),
+        (('?', '--usage'), 'usage', False),
+        (('d', '--debug'), 'debug', False),
+    )
+
+    # based on demos
+    parameterMap = params.parseParams(switchesVarDefaults)
+    server, usage, debug = parameterMap['server'], parameterMap['usage'], parameterMap['debug']
+
+    if usage:
+        params.usage()
+
+    try:
+        serverHost, serverPort = re.split(":", server)
+        serverPort = int(serverPort)
+    except:
+        print("Can't parse server:port from '%s'" % server)
+        sys.exit(1)
+
+    port = (serverHost, serverPort)
+
+    # create socket
+    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listenSocket.connect(port)
+
+    while 1:
+        fileName = input("Enter a filename > ")
+        fileName.strip()
+
+        if fileName == "exit": # terminate
+            sys.exit(0)
+        else:
+            if not fileName:
+                continue
+            elif os.path.exists(PATH + fileName):
+                f = open(PATH + fileName, "rb") # read and binary
+                contents = f.read()
+
+                if len(contents) < 1:
+                    print("Error: File %s is empty" % fileName)
+                    continue
+                
+                framedSend(listenSocket, fileName, contents, debug)
+                # check if server was able to receive the file
+                status = int(listenSocket.recv(1024).decode())
+
+                if status:
+                    print("File %s received by server." % fileName)
+                    sys.exit(0)
+                else:
+                    print("File Transfer Error: File %s was not received by server." % fileName)
+                    sys.exit(1)
+
+            else:
+                print("File Not Found Error: File %s not found!" % fileName)
+
+if __name__ == "__main__":
+    fileClient()
